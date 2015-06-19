9aa5c5533fe0889aeda994ac24d1e87b57b1d877
Leon Wetzel <leonwetzel@gmail.com>
2015-06-19 13:33:17 +0200

    Start met user_overview
    Pagina voor admins mbt weergave van gebruikers

---
+++ admin/user_overview.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leon Wetzel
+ * Date: 19-6-2015
+ * Time: 13:19
+ */
+session_start();
+$title = "Gebruikersoverzicht";
+include'../components/header.php';
+// hier een include naar bestand waar gegevens worden opgehaald
+?>
+
+<h1>Gebruikersoverzicht<h1>
+
+<?php
+// hier het liefste een knop naar het bestand voor het maken van een nieuwe user
+
+// if (gebruikers > 0) {
+//  laat tabel met gegevens zien};
+
+// elke rij bevat ook de knoppen voor extra info en het bewerken van die info
+?>
