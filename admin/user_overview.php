6377dabd1ccedf09941331c592f90ff1d4582802
Leon Wetzel <leonwetzel@gmail.com>
2015-06-20 15:34:23 +0200

    Bijwerken van user_overview, toevoegen van user_info
    Pagina's bijgewerkt. Qua styling moet er nog wat gedaan worden, maar de functionaliteit is er.

--- admin/user_overview.php
+++ admin/user_overview.php
@@ -7,82 +7,64 @@
  */
 
 /**
- * Changelog:
+ * Changelog
  *
  * 19-6-2015 13.00 uur: bestand aangemaakt
- * 19-6-2015 13.27 uur: functies getUsers() en displayUsers() toegevoegd
- * 19-6-2015 14.44 uur: entiteitsnamen aangepast aan huidige staat
- * 19-6-2015 15.18 uur: push naar Sergen
- *
+ * 19-6-2015 13.27 uur: functies toegevoegd
+ * 19-6-2015 13.58 uur: kleine aanpassingen
+ * 19-6-2015 15.17 uur: aanpassingen aan entiteiten gemaakt
+ * 19-6-2015 16.02 uur: aanpassingen gemaakt m.b.t. Meer Informatie-optie
+ * 20-6-2015 11.17 uur: link voor het toevoegen van gebruikers veranderd in knop
  */
 session_start();
 $title = "Gebruikersoverzicht";
 include'../components/header.php';
 // hier een include naar bestand waar gegevens worden opgehaald
-// dit willen we niet hier doen
 ?>
 
 <h1>Gebruikersoverzicht<h1>
         <!-- Knop naar pagina voor het toevoegen van een gebruiker -->
-        <a class="btn btn-default" href="create_user.php" role="button">Nieuwe gebruiker toevoegen</a>
+        <button class="btn btn-default" href="create_user.php">Nieuwe gebruiker toevoegen</button>
 
-<?php
-/**
- * Onderstaande graag later verwerken in een ander bestand
- * Deze functie haalt gebruikers op voor het overzicht
- * @param link
- * @return result
- */
-function getUsers($link)
-{
-    $sql = "SELECT name, id, email, last_login FROM users";
-    $result = mysqli_query($link, $sql);
-    return $result;
-}
-
-/**
- * Onderstaande graag verwerken in een ander bestand
- * Deze functie plaats de gebruikersdata in een tabel
- * @param $result
- */
-function displayUsers($result)
-{
-    if (mysqli_num_rows($result) > 0) {
-        // maak een html-tabel aan
-        ?>
-        <table>
-            <th>ID</th>
-            <th>Naam</th>
-            <th>E-mailadres</th>
-            <th>Laatst ingelogd op</th>
-
-            <?php
-        // per rij data weergeven
-            while ($row = mysqli_fetch_assoc($result)) {
-                // hier data weergeven in tabel
-                echo '<tr>';
-                echo '<td>' . $row["id"] . '</td>';
-                echo '<td>' . $row["name"] . '</td>';
-                echo '<td>' . $row["email"] . '</td>';
-                echo '<td>' . $row["last_login"] . '</td>';
-                echo '<td><button class="btn btn-default" type="button" formaction="user_info.php" name=' . $row["id"] . ' value="Meer informatie"></button></td>';
-                echo '</tr>';
-            }
-            ?>
-        </table>
         <?php
-    } else {
-        // er zijn geen gebruikers gevonden
-        echo "Er zijn geen gebruikers gevonden.";
-    }
+        $sql = "SELECT id, name, email, last_login FROM users";
+        $result = mysqli_query($link, $sql);
 
-    getUsers($link);
-    displayUsers($result);
-}
-// hier het liefste een knop naar het bestand voor het maken van een nieuwe user
+        if (mysqli_num_rows($result) > 0) {
+            // maak een html-tabel aan
+            ?>
+            <form>
+                <table>
+                    <th>ID</th>
+                    <th>Naam</th>
+                    <th>E-mailadres</th>
+                    <th>Laatst ingelogd op</th>
+                    <th>Opties</th>
+                    <?php
+                    // per rij data weergeven
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        // hier data weergeven in tabel
+                        echo '<tr>';
+                        echo '<td>' . $row["id"] . '</td>';
+                        echo '<td>' . $row["name"] . '</td>';
+                        echo '<td>' . $row["email"] . 'a</td>';
+                        echo '<td>' . $row["last_login"] . '</td>';
+                        echo '<td><button class="btn btn-default" type="submit" formmethod="post" formaction="user_info.php" name="user_info" value=' . $row["id"] . '>Meer informatie</button></td>';
+                        echo '</tr>';
+                    }
+                    ?>
+                </table>
+            </form>
+        <?php
+        } else {
+            // er zijn geen gebruikers gevonden
+            echo "Er zijn geen gebruikers gevonden.";
+        }
 
-// if (gebruikers > 0) {
-//  laat tabel met gegevens zien};
+        // hier het liefste een knop naar het bestand voor het maken van een nieuwe user
 
-// elke rij bevat ook de knoppen voor extra info
-?>
+        // if (gebruikers > 0) {
+        //  laat tabel met gegevens zien};
+
+        // elke rij bevat ook de knoppen voor extra info
+        ?>
