db3b1c51c9311a1dfc7d50b4916671085d2f0d09
PaulKoning <paulkoning@live.nl>
2015-06-22 14:44:27 +0200

    I don't even github
    tot ziens

--- admin/companies.php
+++ admin/companies.php
@@ -16,14 +16,14 @@ $result = mysqli_query($link, $query);
 while($row=mysqli_fetch_assoc($result)){
 	echo('
 		<tr>
-			<td><input type="text" name="cname" value=".$row['name']."></td>
-			<td><input type="text" name="cname" value=".$row['branch']."></td>
-			<td><input type="text" name="cname" value=".$row['contact']."></td>
-			<td><input type="text" name="cname" value=".$row['info']."></td>
+			<td><input type="text" name="cname" value=".$row["name"]."></td>
+			<td><input type="text" name="cbranch" value=".$row["branch"]."></td>
+			<td><input type="text" name="ccontact" value=".$row["contact"]."></td>
+			<td><input type="text" name="cinfo" value=".$row["info"]."></td>
 		</tr>
 ');
-	
 ?>
+</table>
 <?php
 include'components/footer.php';
 ?>
