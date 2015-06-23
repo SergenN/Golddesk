ea31b5631d1323a19824282bbae7dc88f9bc04c7
Leon Wetzel <leonwetzel@gmail.com>
2015-06-23 11:59:58 +0200

    Nieuwe bestanden voor bewerken van gebruiker

---
+++ admin/edit_user.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leon Wetzel
+ * Date: 19-6-2015
+ * Time: 16:03
+ */
+
+/**
+ * Changelog
+ *
+ * 19-6-2015 16.03 uur: bestand aangemaakt
+ */
+session_start();
+$title = "Gebruiker bewerken";
+$id = $_POST["user_data"];
+include'../components/header.php';
+$secure = 9;
+// hier een include naar bestand waar gegevens worden opgehaald
+?>
+
+<html>
+<body>
+<div class="container">
+    <form action="edited_user.php" method="post">
+        <div class="col-md-6">
+            <?php
+            $sql = "SELECT id, name, password, email, company, function, level, supervisor, firstname, lastname FROM users WHERE id = $id ";
+            $result = mysqli_query($link, $sql);
+            while ($row = mysqli_fetch_assoc($result)) {
+                $id = $row["id"];
+                $username = $row["name"];
+                $password = $row["password"];
+                $email = $row["email"];
+                $company = $row["company"];
+                $function = $row["function"];
+                $level = $row["level"];
+                $supervisor = $row["supervisor"];
+                $firstname = $row["firstname"];
+                $lastname = $row["lastname"];
+            }
+            ?>
+            <div class="form-group">
+                <label for="InputUsername">Gebruikersnaam*</label>
+                <input type="text" class="form-control" id="InputUsername" name="InputUsername" value="<?php echo $username; ?>" placeholder="<?php echo $username; ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="InputName">Voornaam*</label>
+                <input type="text" class="form-control" id="InputName" name="InputName" value="<?php echo $firstname; ?>" placeholder="<?php echo $firstname; ?>">
+            </div>
+            <div class="form-group">
+                <label for="InputSurname">Achternaam*</label>
+                <input type="text" class="form-control" id="InputSurname" name="InputSurname" value="<?php echo $lastname; ?>" placeholder="<?php echo $lastname; ?>">
+            </div>
+            <div class="form-group">
+                <label for="InputEmail">E-mailadres*</label>
+                <input type="email" class="form-control" id="InputEmail" name="InputEmail" value="<?php echo $email; ?>" placeholder="<?php echo $email; ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="InputCompany">Bedrijf</label>
+                <select class="form-control" name="InputCompany">
+                    <option value="0">Scholengemeenschap De Hondsrug (0)</option>
+                    <?php
+                    // onderstaande query graag in een ander bestand stoppen
+                    $sql = "SELECT name, id FROM companies ORDER BY id";
+                    $result = mysqli_query($link, $sql);
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo '<option value=' . $row["id"] . '>' . $row["name"] . ' (' . $row["id"] . ')</option>';
+                    }
+                    ?>
+                </select>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-group">
+                <label for="InputFunction">Functie</label>
+                <input type="text" class="form-control" id="InputFunction" name="InputFunction" value="<?php echo $function; ?>" placeholder="<?php echo $function; ?>">
+            </div>
+            <div class="form-group">
+                <label for="InputLevel">Toegangsniveau*</label>
+                <select class="form-control" name="InputLevel">
+                    <option value="0">0 (gewone gebruiker)</option>
+                    <option value="1">1 (eerstelijns)</option>
+                    <option value="2">2 (tweedelijns)</option>
+                    <option value="3">3 (derdelijns)</option>
+                    <option value="9">9 (administrator)</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="InputSupervisor">Supervisor</label>
+                <select class="form-control" name="InputSupervisor">
+                    <?php
+                    // onderstaande query graag in een ander bestand stoppen
+                    $sql = "SELECT name, id FROM users ORDER BY id";
+                    $result = mysqli_query($link, $sql);
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo '<option value=' . $row["id"] . '>' . $row["name"] . ' (' . $row["id"] . ')</option>';
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="InputPassword">Nieuw wachtwoord</label>
+                <input type="password" class="form-control" id="InputPassword" name="InputPassword" placeholder="Wachtwoord">
+            </div>
+            <div class="form-group">
+                <label for="SecondInputPassword">Nieuw wachtwoord herhalen</label>
+                <input type="password" class="form-control" id="SecondInputPassword" name="SecondInputPassword" placeholder="Wachtwoord">
+            </div>
+            <button type="submit" class="btn btn-default" name="edited_data" value="<?php echo $id; ?>">Gegevens wijzigen</button>
+        </div>
+    </form>
+</div>
+</body>
+</html>
---
+++ admin/edited_user.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Leon
+ * Date: 22-6-2015
+ * Time: 09:37
+ */
+
+/**
+ * Changelog
+ *
+ * 22-6-2015 9.37 uur: Bestand aangemaakt
+ */
+
+session_start();
+$title = "Gebruiker bewerken";
+$id = $_POST["edited_data"];
+include'../components/header.php';
+$secure = 9;
+// hier een include naar bestand waar gegevens worden opgehaald
+$foutmeldingen = array();
+
+// post ontvangen
+$username = $_POST["InputUsername"];                    // verplicht
+$name = $_POST["InputName"];
+$surname = $_POST["InputSurname"];
+$email = $_POST["InputEmail"];                          // verplicht
+$company = $_POST["InputCompany"];
+$function = $_POST["InputFunction"];
+$level = $_POST["InputLevel"];                          // verplicht
+$supervisor = $_POST["InputSupervisor"];
+$wachtwoord = $_POST["InputPassword"];                  // verplicht
+$wachtwoord2 = $_POST["SecondInputPassword"];           // verplicht
+
+// oude gegevens van gebruiker
+$sql = "SELECT id, name, password, email, company, function, level, supervisor, firstname, lastname FROM users WHERE id = $id ";
+$result = mysqli_query($link, $sql);
+while ($row = mysqli_fetch_assoc($result)) {
+    $id = $row["id"];
+    $us = $row["name"];
+    $password = $row["password"];
+    $em = $row["email"];
+    $cp = $row["company"];
+    $ft = $row["function"];
+    $lvl = $row["level"];
+    $sv = $row["supervisor"];
+    $fn = $row["firstname"];
+    $ln = $row["lastname"];
+}
+
+// data voorbereiden
+if(!empty($username)) {
+    $username = mysqli_real_escape_string($link, strtolower(trim($username)));
+} else {
+    $foutmeldingen[] = "Gebruikersnaam is ongeldig.";
+}
+
+if(!empty($name)) {
+    $name = mysqli_real_escape_string($link, ucfirst(trim($name)));
+} else {
+    $foutmeldingen[] = "Voornaam is ongeldig.";
+}
+
+if(!empty($surname)) {
+    $surname = mysqli_real_escape_string($link, trim($surname));
+} else {
+    $foutmeldingen[] = "Achternaam is ongeldig.";
+}
+
+if(!empty($email)) {
+    $email = mysqli_real_escape_string($link, strtolower(trim($email)));
+} else {
+    $foutmeldingen[] = "E-mailadres is ongeldig.";
+}
+
+if(!empty($company)) {
+    $company = trim($company);
+} else {
+    $company = 0;
+}
+
+if(!empty($function)) {
+    $function = mysqli_real_escape_string($link, ucfirst(trim($function)));
+} else {
+    $function = "Onbekend";
+}
+
+if(!empty($supervisor)) {
+    $supervisor = trim($supervisor);
+} else {
+    $supervisor = 0;
+}
+
+if(strcmp($wachtwoord, $wachtwoord2) !== 0) {
+    // ingevoerde wachtworden komen niet overeen
+    $foutmeldingen[] = "De ingevoerde wachtwoorden komen niet overeen.";
+    $check = 0;
+} elseif(empty($wachtwoord) && empty($wachtwoord2)) {
+    // wachtwoord is onveranderd
+    $wachtwoord = $password;
+    $check = 1;
+} else {
+    // nieuw wachtwoord ingevoerd
+    $wachtwoord = mysqli_real_escape_string($link, $wachtwoord);
+    $wachtwoord = md5($wachtwoord);
+    $check = 1;
+}
+
+// query die data in de db moet zetten
+if($check) {
+    $sql = "UPDATE users SET name = '$username', password = '$password', email = '$email', company = '$company', function = '$function', level = $level, supervisor = $supervisor, firstname = '$name', lastname = '$surname' WHERE id = $id ";
+}
+?>
+
+<html>
+<body>
+<h1>
+    <?php
+    if (mysqli_query($link, $sql)) {
+        echo "Gebruiker bewerken geslaagd!";
+    } elseif(!empty($foutmeldingen)) {
+        echo "Er is iets fout gegaan:";
+    } else {
+        echo "Er is iets fout gegaan tijdens het bewerken van de gegevens van een gebruiker.";
+    }
+    ?>
+</h1>
+<?php
+
+foreach($foutmeldingen as $foutmelding) {
+    echo "- " . $foutmelding . "<br>";
+}
+
+if($check) {
+    echo 'Keer terug naar de <a href="admin/index.php">homepagina</a>.';
+}
+?>
+</body>
+</html>
